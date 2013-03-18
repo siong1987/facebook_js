c616d0f7d60230140eab472a15a678066bd7aa76
Teng Siong Ong <siong1987@gmail.com>
2013-03-17 21:00:48 -0700

    Add block to facebook_js.

--- app/helpers/facebook_js_helper.rb
+++ app/helpers/facebook_js_helper.rb
@@ -1,12 +1,19 @@
 module FacebookJsHelper
-  def facebook_js
+  def facebook_js(&block)
+    if block_given?
+      block = capture(&block)
+    else
+      block = nil
+    end
+
     render partial: 'facebook_js_helper/facebook', locals: {
       app_id: FacebookJs.app_id,
       domain: FacebookJs.domain,
       status: FacebookJs.status,
       cookie: FacebookJs.cookie,
       xfbml: FacebookJs.xfbml,
-      debug: FacebookJs.debug
+      debug: FacebookJs.debug,
+      block: block
     }
   end
 end
--- test/helpers/facebook_js_helper_test.rb
+++ test/helpers/facebook_js_helper_test.rb
@@ -14,26 +14,30 @@ class FacebookJsHelperTest < ActionView::TestCase
   end
 
   test 'facebook_js should render the proper default content' do
-    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:true});//AdditionalinitializationcodesuchasaddingEventListenersgoeshere};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:true});};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
   end
 
   test 'facebook_js should render the proper content with proper status' do
     FacebookJs.status = false
-    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:false,cookie:true,xfbml:true});//AdditionalinitializationcodesuchasaddingEventListenersgoeshere};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:false,cookie:true,xfbml:true});};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
   end
 
   test 'facebook_js should render the proper content with proper cookie' do
     FacebookJs.cookie = false
-    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:false,xfbml:true});//AdditionalinitializationcodesuchasaddingEventListenersgoeshere};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:false,xfbml:true});};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
   end
 
   test 'facebook_js should render the proper content with proper xfbml' do
     FacebookJs.xfbml = false
-    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:false});//AdditionalinitializationcodesuchasaddingEventListenersgoeshere};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:false});};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", facebook_js.split(/\n/).join("").delete(" ")
   end
 
   test 'facebook_js should render the proper content with proper debug' do
     FacebookJs.debug = true
-    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:true});//AdditionalinitializationcodesuchasaddingEventListenersgoeshere};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,true));</script>", facebook_js.split(/\n/).join("").delete(" ")
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:true});};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,true));</script>", facebook_js.split(/\n/).join("").delete(" ")
+  end
+
+  test 'facebook_js should render the proper content with block' do
+    assert_equal "<divid=\"fb-root\"></div><script>window.fbAsyncInit=function(){FB.init({appId:'12345',channelUrl:'//localhost:3000/channel.html',status:true,cookie:true,xfbml:true});function(){}};(function(d,debug){varjs,id='facebook-jssdk',ref=d.getElementsByTagName('script')[0];if(d.getElementById(id)){return;}js=d.createElement('script');js.id=id;js.async=true;js.src=\"//connect.facebook.net/en_US/all\"+(debug?\"/debug\":\"\")+\".js\";ref.parentNode.insertBefore(js,ref);}(document,false));</script>", (facebook_js { 'function(){}' } ).split(/\n/).join("").delete(" ")
   end
 end
